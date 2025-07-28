c6018f564aab7b0004c3e770988e6136341fb200
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-27 23:27:56 -0500

    database

---
+++ app/core/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine # type: ignore
+from sqlalchemy.ext.declarative import declarative_base # type: ignore
+from sqlalchemy.orm import sessionmaker # type: ignore
+from app.core.config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine
+)
+
+Base = declarative_base()
